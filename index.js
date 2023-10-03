e92167d63191a42b30c19cac9b9dbdb5313507d9
20JorgeCardenas05 <jcardenas74@ucol.mx>
2023-10-02 19:03:39 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Declaración de variables en JavaScript moderno 
+// var numero = 5;
+// var numero = 4;
+// let numero = 5;
+// numero = 4;
+// const constante = 8;
+// constante = 10;
+
+// Alerta en JavaScript
+// alert( "Esta es una alerta de JavaScript");
+// console.log("Esto es JavaScript desde HTML");
+// console.log(numero);
+// console.log(constante);
+
+// Concatenación
+let nombre = "Jorge";
+let apellido = "Cardenas";
+console.log("Mi nombre es: "+nombre+" "+apellido);
+document.write("Mi nombre es: "+nombre+" "+apellido);
+
+let seleccion = document.querySelector('#seccion1');
+
+console.log(seleccion);
+
+seleccion.innerHTML = ``;
